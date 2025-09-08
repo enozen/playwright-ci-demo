b1d9d3ca7655fa984bbdb022bfc6470e4d9dd905
Hendri Zeneli <hendrizeneli@yahoo.com>
2025-09-08 13:26:09 +0200

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,3 +1,5 @@
+import { test, expect } from '@playwright/test';
+
 test('should interact with form elements', async ({ page }) => {
   await page.goto('https://demoqa.com/text-box');
   await page.fill('#userName', 'John Doe');
